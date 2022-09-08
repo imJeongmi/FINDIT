f4b6c56485d12de0fbb75055791f206cede88c9f
최희선 <asdfmelody@naver.com>
2022-09-08 15:43:14 +0900

    feat: dependencies setting

---
+++ BE/findit/src/main/java/a203/findit/FinditApplication.java
@@ -0,0 +1,13 @@
+package a203.findit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinditApplication.class, args);
+	}
+
+}
---
+++ BE/findit/src/test/java/a203/findit/FinditApplicationTests.java
@@ -0,0 +1,13 @@
+package a203.findit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
