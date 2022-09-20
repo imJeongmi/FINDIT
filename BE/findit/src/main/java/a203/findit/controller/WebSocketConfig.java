47ff586a065256a7d30906bda000a80942c43e4a
최희선 <asdfmelody@naver.com>
2022-09-20 14:45:15 +0900

    feat: Socket

--- BE/findit/src/main/java/a203/findit/controller/WebSocketConfig.java
+++ BE/findit/src/main/java/a203/findit/controller/WebSocketConfig.java
@@ -1,12 +1,10 @@
-package a203.findit.config;
+package a203.findit.controller;
 
-import a203.findit.handler.MySocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,8 +18,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("wss")
-                .setAllowedOrigins("*");
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*");
+//                .setInterceptors(httpSessionIdHandshakeInterceptor());
     }
+
+//    @Bean
+//    public HttpSessionIdHandshakeInterceptor httpSessionIdHandshakeInterceptor() {
+//        return new HttpSessionIdHandshakeInterceptor();
+//    }
 
 }
