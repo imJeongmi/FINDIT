8721dbae659fe646db411d832fff007de698b7a5
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-09-22 09:45:52 +0900

    feat: Add EnterCode page template

---
+++ FE/findit/src/components/page/EnterVerificationCode.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EnterCode from "components/atom/EnterCode";
+import { Box } from "@mui/system";
+import Modal from "components/atom/Modal";
+
+
+export default function EnterVerificationCode() {
+  return (
+    <Box>
+      <Modal></Modal>
+    </Box>
+  );
+}
