b9444e48b602ec486d2c638417ea09d76e2a9026
jina jeong <jina092400@gmail.com>
2022-09-26 22:54:24 +0900

    [FIX] Restore App.js background img

--- FE/findit/src/App.js
+++ FE/findit/src/App.js
@@ -2,18 +2,28 @@ import React from "react";
 
 import { Box } from "@mui/system";
 import RouterConfiguration from "configs/router";
-import Tutorial from "components/page/Tutorial";
+
+import Background from "static/background.png";
 
 import "./App.css";
 
 const BodyStyle = {
-  marginTop: "4vh",
+  // marginTop: "4vh",
+  backgroundImage: `url(${Background})`,
+  backgroundSize: "cover",
+  height: "100vh",
+  width: "100vw",
+  position: "fixed",
+  top: 0,
+  left: 0,
 };
 
 function App() {
-  return <Box sx={BodyStyle}>
-    <RouterConfiguration />
-  </Box>;
+  return (
+    <Box sx={BodyStyle}>
+      <RouterConfiguration />
+    </Box>
+  );
 }
 
 export default App;
