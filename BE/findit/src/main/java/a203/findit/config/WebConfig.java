657787a63a507be12ce64d8c2d021f281e45035a
Denny-Ko <donghar@naver.com>
2022-09-28 17:57:31 +0900

    fix: CORS ERROR

--- BE/findit/src/main/java/a203/findit/config/WebConfig.java
+++ BE/findit/src/main/java/a203/findit/config/WebConfig.java
@@ -18,7 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*")
-//                .exposedHeaders("*")
                 .maxAge(6000);
     }
 }
