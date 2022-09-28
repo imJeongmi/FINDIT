ea587b6ebf1af5b1651bd4d81e3235c7a7080769
Jeongmi Park <qkrwjdal419@gmail.com>
2022-09-28 17:09:07 +0900

    Edit: add ExitButton to Playing page

---
+++ FE/findit/src/components/atom/ExitButton.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { styled, Box } from "@mui/system";
+
+import ExitIcon from "static/exit.png"
+
+const ButtonBox = styled(Box)(
+    () => `
+    width: 6vw;
+    height: 6vw;
+    background-color: white;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `
+)
+
+export default function ExitButton() {
+    return (
+      <ButtonBox>
+        <img src={ExitIcon} alt="exitIcon" width="20px" height="20px"/>
+      </ButtonBox>
+    );
+}
