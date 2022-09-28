6b111f95aa4a3f6bd77a20c5cab58ad86dcecbc8
jina jeong <jina092400@gmail.com>
2022-09-28 23:09:07 +0900

    feat: Add treasure selecting function

--- FE/findit/src/components/page/SelectTreasure.js
+++ FE/findit/src/components/page/SelectTreasure.js
@@ -1,6 +1,8 @@
 import { Box, styled } from "@mui/system";
+import { getTreasureList } from "api/treasure";
 import CustomButton from "components/atom/CustomButton";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 import CustomText from "../atom/CustomText";
 import TreasureItem from "../atom/TreasureItem";
@@ -24,15 +26,47 @@ const AddTreasureButton = styled(Box)(
   border-radius: 25%;
   box-shadow: 2px 2px 10px 1px #E2E2E2;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   float: left;
+  background-color: white;
   `,
 );
 
 export default function SelectTreasure() {
-  // treasure_list 받아오면
-  // mytreasure이면 분기 처리할 것
+  // 더미데이터 api연결 후 삭제할 것
+  const [treasureList, setTreasureList] = useState([
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+    "https://placeimg.com/100/100/any",
+  ]);
+  const { gameId } = useParams();
+  const [selectedTreasures, setSelectedTreasures] = useState([]);
+
+  useEffect(() => {
+    getTreasureList(getTreasureListSuccess, getTreasureListFail);
+  }, [gameId]);
+
+  function getTreasureListSuccess(res) {
+    setTreasureList(res.data);
+  }
+
+  function getTreasureListFail(err) {
+    console.log("보물 목록 요청 실패", err);
+  }
+
+  function selectTreasure(treasure) {
+    setSelectedTreasures([...selectedTreasures, treasure]);
+  }
+
   return (
     <Box sx={BoxStyle}>
       <Box sx={{ marginTop: "5vh" }}>
@@ -46,6 +80,10 @@ export default function SelectTreasure() {
       </Box>
       <Box sx={TreasureBoxStyle}>
         {/* 보물 리스트 받아온 후, map으로 변경 */}
+        {/* <TreasureItem
+          src="https://placeimg.com/100/100/any"
+          onClick={() => selectTreasure(treasure)}
+        ></TreasureItem>
         <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
         <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
         <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
@@ -54,12 +92,17 @@ export default function SelectTreasure() {
         <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
         <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
         <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
-        <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
-        <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem>
+        <TreasureItem src="https://placeimg.com/100/100/any"></TreasureItem> */}
+        {treasureList.map((treasure, key) => (
+          <TreasureItem src={treasure} />
+        ))}
         {/* Onclick 달아야 함 */}
         <AddTreasureButton>
-          <CustomText size="large" variant="primary">
+          <CustomText size="xxxl" variant="secondary">
             +
+          </CustomText>
+          <CustomText size="xxs" variant="secondary">
+            나만의 보물 추가
           </CustomText>
         </AddTreasureButton>
       </Box>
