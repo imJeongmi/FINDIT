98439407a272b0c352feb85ab3f18d993279cb52
jina jeong <jina092400@gmail.com>
2022-09-28 16:17:42 +0900

    feat: Update enter code font

--- FE/findit/src/components/atom/EnterCode.js
+++ FE/findit/src/components/atom/EnterCode.js
@@ -1,8 +1,7 @@
-import { fontFamily } from "@mui/system";
 import { useState } from "react";
 import ReactInputVerificationCode from "react-input-verification-code";
 
-import './EnterCode.css'
+import "./EnterCode.css";
 
 export default function EnterCode() {
   const [value, setValue] = useState("");
@@ -15,14 +14,14 @@ export default function EnterCode() {
         value={value}
         placeholder={null}
         length={6}
-        onChange={(newValue) => {
-          setValue(newValue)
-        style={{ fontFamily: "GmarketSansMedium"}};
+        onChange={newValue => {
+          setValue(newValue);
 
           // if (newValue !== "") {
           //   setError(null);
           // }
         }}
+        style={{ fontFamily: "GmarketSansBold" }}
       />
 
       {/* 버튼 */}
