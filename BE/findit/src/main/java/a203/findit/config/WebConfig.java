b1c603f9b91e05a6ec5c795ba6ad17b9c61db527
최희선 <asdfmelody@naver.com>
2022-09-28 16:04:43 +0900

    fix: CORS error

--- BE/findit/src/main/java/a203/findit/config/WebConfig.java
+++ BE/findit/src/main/java/a203/findit/config/WebConfig.java
@@ -1,23 +1,24 @@
-//package a203.findit.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    private String frontUrl = "http://localhost:3000";
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-////                .allowedOriginPatterns("**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-//                .allowedHeaders("*")
+package a203.findit.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private String frontUrl = "http://localhost:3000";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+//                .allowedOriginPatterns("**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
 //                .allowCredentials(true)
-//                .maxAge(6000);
-//    }
-//}
+                .exposedHeaders("*")
+                .maxAge(6000);
+    }
+}
