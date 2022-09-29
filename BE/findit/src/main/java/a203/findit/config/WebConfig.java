b6259e8ab0e023c750896fa806eb7e6699926dbb
Denny-Ko <donghar@naver.com>
2022-09-29 13:45:02 +0900

    fix: Cors

--- BE/findit/src/main/java/a203/findit/config/WebConfig.java
+++ BE/findit/src/main/java/a203/findit/config/WebConfig.java
@@ -10,15 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
 
 //    private String frontUrl = "http://localhost:3000";
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-//                .allowedOrigins("frontUrl")
-//                .allowedOriginPatterns("*")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-//                .allowCredentials(true)
-                .allowedHeaders("*")
-                .maxAge(6000);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+////                .allowedOrigins("frontUrl")
+////                .allowedOriginPatterns("*")
+//                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+////                .allowCredentials(true)
+//                .allowedHeaders("*")
+//                .maxAge(6000);
+//    }
 }
