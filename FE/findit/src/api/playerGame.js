00ee8767e3176822b2f8dca870cd686dead8617d
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-09-30 15:58:22 +0900

    delete: Unused api

--- FE/findit/src/api/playerGame.js
+++
@@ -1,6 +0,0 @@
-// import { getWebsocket } from "helper/websocket";
-// import PlayerApi from "./PlayerApi";
-
-// function enterGame(enterCode, profileImg, nickname, success, fail) {}
-
-// export { enterGame };
