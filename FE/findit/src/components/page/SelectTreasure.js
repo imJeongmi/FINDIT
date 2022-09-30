248d1d5af1d6d7a79b4c991bed0b359e26be125f
jina jeong <jina092400@gmail.com>
2022-09-30 12:01:05 +0900

    feat: Update selectTreasure page

--- FE/findit/src/components/page/SelectTreasure.js
+++ FE/findit/src/components/page/SelectTreasure.js
@@ -162,6 +162,7 @@ export default function SelectTreasure() {
                 // selectedItems={selectedItems}
                 // selectedItemHandler={selectedItemHandler}
                 alt="treasure"
+                isReadPage="true"
               />
             </Box>
           ))}
