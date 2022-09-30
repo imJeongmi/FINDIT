334c61af243c7708fcdd7483689054645afc223f
Denny-Ko <donghar@naver.com>
2022-09-30 14:43:22 +0900

    fix: CORS

--- BE/findit/src/main/java/a203/findit/config/SecurityConfig.java
+++ BE/findit/src/main/java/a203/findit/config/SecurityConfig.java
@@ -105,11 +105,12 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("https://findit.life");
-        configuration.addAllowedOrigin(frontUrl);
+//        configuration.addAllowedOrigin("https://findit.life");
+//        configuration.addAllowedOrigin(frontUrl);
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true); // 내 서버가 응답을 할 때 Json 을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
+//        configuration.setAllowCredentials(true); // 내 서버가 응답을 할 때 Json 을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
