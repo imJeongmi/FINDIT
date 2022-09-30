37f9de092dcd9ea680d403c084ceb1d13908aae4
Denny-Ko <donghar@naver.com>
2022-09-30 15:10:54 +0900

    feat: CreateTreasures

--- BE/findit/src/main/java/a203/findit/model/repository/TreasureRepository.java
+++ BE/findit/src/main/java/a203/findit/model/repository/TreasureRepository.java
@@ -3,5 +3,9 @@ package a203.findit.model.repository;
 import a203.findit.model.entity.Treasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface TreasureRepository extends JpaRepository<Treasure, Long> {
+    List<Treasure> findByIdIn(Collection<Long> ids);
 }
