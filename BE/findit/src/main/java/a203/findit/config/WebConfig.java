2fdc0437619dcbe3734dc68d487afaf6eb5f5855
Denny-Ko <donghar@naver.com>
2022-09-30 16:40:31 +0900

    fix: CORS

--- BE/findit/src/main/java/a203/findit/config/WebConfig.java
+++ BE/findit/src/main/java/a203/findit/config/WebConfig.java
@@ -19,11 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
 //		Set max age to 1800 seconds (30 minutes).
         registry.addMapping("/**")
 //			.allowedOrigins("*")
-                .allowedOriginPatterns("*")
-//                .allowedOrigins(frontUrl)
+//                .allowedOriginPatterns("*")
+                .allowedOrigins("https://findit.life")
+                .allowedOrigins(frontUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true) // 쿠키 허용
+//                .allowCredentials(true) // 쿠키 허용
                 .maxAge(6000);
     }
 }
