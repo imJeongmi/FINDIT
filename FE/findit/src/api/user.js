54abe2667c008fb9a52ccfa80e380241fdf39583
Jeongmi Park <qkrwjdal419@gmail.com>
2022-10-03 03:17:08 +0900

    feat: Edit AddTreasure page

--- FE/findit/src/api/user.js
+++ FE/findit/src/api/user.js
@@ -1,9 +1,10 @@
 import UserApi from "./UserApi";
+import axios from "axios";
 
 function requestJoin(id, pw, nickname, success, fail) {
   UserApi.post("users", { id: id, pw: pw, nickname: nickname }).then(success).catch(fail);
 }
-
+ 
 function requestLogin(id, pw, success, fail) {
   UserApi.post("users/login", { id: id, pw: pw }).then(success).catch(fail);
 }
@@ -12,4 +13,23 @@ function requestLogout(success, fail) {
   UserApi.post("users/logout").then(success).catch(fail);
 }
 
-export { requestJoin, requestLogin, requestLogout };
+function requestUpload(data, success, fail) {
+  axios
+    .post(
+      "https://findit.life/fast/check",
+      {
+        game_id: data.game_id,
+        file: data.file,
+      },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Accept: "/",
+        },
+      },
+    )
+    .then(success)
+    .catch(fail);
+}
+
+export { requestJoin, requestLogin, requestLogout, requestUpload };
