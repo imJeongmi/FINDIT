b8d3c6f83761ded181fb249f38446659a15274f7
최희선 <asdfmelody@naver.com>
2022-10-03 14:21:09 +0900

    feature/be/refactoring

--- BE/findit/src/main/java/a203/findit/service/GameService.java
+++ BE/findit/src/main/java/a203/findit/service/GameService.java
@@ -5,15 +5,4 @@ import org.springframework.http.ResponseEntity;
 import java.util.Map;
 
 public interface GameService {
-
-
-    Map<String, String> startGame(String roomId, String username);
-
-    ResponseEntity getGameDetail(String roomId, String username);
-
-    ResponseEntity findTreasure(String roomId, String username);
-
-    ResponseEntity getRank(String roomId, String username);
-
-    ResponseEntity getScores(String roomId, String username);
 }
