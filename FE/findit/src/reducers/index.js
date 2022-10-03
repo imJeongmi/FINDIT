7d0c2cd8f690da8e90cd4a322ba2ee2e94bf33d5
jina jeong <jina092400@gmail.com>
2022-10-03 16:08:06 +0900

    feat: Update redux-persist setting

--- FE/findit/src/reducers/index.js
+++ FE/findit/src/reducers/index.js
@@ -2,14 +2,14 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import user from "stores/user";
+import user from "reducers/user";
 
 const persistConfig = {
   key: "root",
   // localStorage에 저장합니다.
   storage,
   // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
-  whitelist: ["auth"],
+  // whitelist: ["user"],
   // blacklist -> 그것만 제외합니다
 };
 
