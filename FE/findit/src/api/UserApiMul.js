14d7b4faa0a1cb9fd0fb4aab42565420a657535d
Denny-Ko <donghar@naver.com>
2022-10-04 21:30:30 +0900

    fix: URL

--- FE/findit/src/api/UserApiMul.js
+++ FE/findit/src/api/UserApiMul.js
@@ -6,8 +6,8 @@ axios.defaults.withCredentials = true;
 // configuration
 const UserApiMul = axios.create({
     // baseURL: "https://j7a203.p.ssafy.io/api/v1/",
-    // baseURL: "https://findit.life/api/v1/",
-    baseURL: "http://localhost:8399/api/v1/",
+    baseURL: "https://findit.life/api/v1/",
+    // baseURL: "http://localhost:8399/api/v1/",
     headers: {
         "Content-Type": "multipart/form-data",
     },
