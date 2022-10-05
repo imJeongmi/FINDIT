751baa76ffda6cd0338a94663b785cbbf0bef3a6
최희선 <asdfmelody@naver.com>
2022-10-05 09:04:20 +0900

    fix: Timezone

--- BE/findit/src/main/java/a203/findit/FinditApplication.java
+++ BE/findit/src/main/java/a203/findit/FinditApplication.java
@@ -3,9 +3,16 @@ package a203.findit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FinditApplication {
-
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FinditApplication.class, args);
 	}
